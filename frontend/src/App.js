4147f8b2b207ba7bdce402bcf1dc9553743963d4
raullopeslopez <78404764+raullopeslopez@users.noreply.github.com>
2023-07-29 17:14:23 +0200

    first api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header';
 import Search from './components/Search';
 
+const UNPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 const App = () => {
   const [word, setWord] = useState('')
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word)
+    fetch(`https://api.unsplash.com/photos/random?query=${word}&client_id=${UNPLASH_KEY}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
   }
 
-  console.log(word)
+  console.log(process.env);
   
   return (
     <div>
